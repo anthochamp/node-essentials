a77ec3d04398eaab30da8145391922afc3bf6592
Anthony Champagne <dev@anthonychampagne.fr>
2024-09-22 20:09:57 +0200

    add markdownlint config

---
+++ packages/markdownlint-cli2-config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+	config: require("@ac-essentials/markdownlint-config"),
+
+	gitignore: true,
+
+	globs: ["**/*.{md,mdx}"],
+};
---
+++ packages/markdownlint-config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+	default: true,
+
+	// MD013/line-length
+	// Line length : https://github.com/DavidAnson/markdownlint/blob/v0.35.0/doc/md013.md
+	MD013: false,
+};
