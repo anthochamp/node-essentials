38faff226e9e76153d139f4e9aaf738b9cf17657
Anthony Champagne <dev@anthonychampagne.fr>
2025-11-14 19:44:07 +0100

    vitest: rem logHeapUsage, move typecheck to misc-util

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		projects: ["packages/*"],
-		root: __dirname,
 		silent: process.env.CI ? "passed-only" : false,
-		logHeapUsage: true,
-		typecheck: {
-			enabled: true,
-		},
 	},
 });
