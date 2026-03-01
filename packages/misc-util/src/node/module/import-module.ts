fe3fb966ee0e41d633864258fe66f64dbf0422c2
Anthony Champagne <dev@anthonychampagne.fr>
2026-03-01 17:50:45 +0100

    remove biome false positive ignore comments

--- packages/misc-util/src/node/module/import-module.ts
+++ packages/misc-util/src/node/module/import-module.ts
@@ -13,7 +13,6 @@ export async function importModule<T>(
 	id: string,
 	resolvePaths?: string[],
 ): Promise<T> {
-	// biome-ignore lint/nursery/noUnnecessaryConditions: false positive
 	if (resolvePaths && resolvePaths.length > 0) {
 		id = require.resolve(id, { paths: resolvePaths });
 	}
