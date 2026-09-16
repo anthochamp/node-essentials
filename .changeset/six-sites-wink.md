54af59ff48898edf8baf5dd429b3e5fcf8eb737e
anthochamp <dev@anthonychampagne.fr>
2026-09-16 18:11:52 +0200

    fix markdownlint-cli2 config

---
+++ .changeset/six-sites-wink.md
@@ -0,0 +1,5 @@
+---
+"@ac-kit/markdownlint-cli2-config": minor
+---
+
+Remove banner, progress and summary and add ignores patterns
--- packages/markdownlint-cli2-config/index.js
+++ packages/markdownlint-cli2-config/index.js
@@ -1,7 +1,12 @@
 module.exports = {
 	config: require("@ac-kit/markdownlint-config"),
 
+	noBanner: true,
+	noProgress: true,
+	showFound: false,
 	gitignore: true,
 
 	globs: ["**/*.{md,mdx}"],
+
+	ignores: ["**/node_modules/**", "**/.git/**", "**/.changeset/**"],
 };
